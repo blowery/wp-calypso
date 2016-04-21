f1687262b6f66ef763d51b80f04941bde6dff2ca
Jonathan Sadowski <sadowski@automattic.com>
2016-04-21 11:45:46 -0500

    Add a temporary section for testing push notification library

--- client/sections.js
+++ client/sections.js
@@ -161,6 +161,13 @@ sections = [
 		module: 'my-sites/plans',
 		secondary: true,
 		group: 'sites'
+	},
+	{
+		name: 'testing',
+		paths: [ '/testing' ],
+		module: 'my-sites/testing',
+		secondary: true,
+		group: 'sites'
 	}
 ];
 
