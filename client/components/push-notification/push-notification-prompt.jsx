741123f27ce762f486b017f338b9b4dd0e57e474
Jonathan Sadowski <sadowski@automattic.com>
2016-05-02 07:41:33 -0500

    Add missing push notification component

---
+++ client/components/push-notification/push-notification-prompt.jsx
@@ -0,0 +1,113 @@
+/**
+ * External dependencies
+ */
+var React = require( 'react' ),
+	debug = require( 'debug' )('asdf');
+
+/**
+ * Internal dependencies
+ */
+var Notice = require( 'components/notice' ),
+	observe = require( 'lib/mixins/data-observe' );
+
+module.exports = React.createClass( {
+	displayName: 'PushNotificationPrompt',
+
+	mixins: [ observe( 'pushNotifications' ) ],
+
+	getInitialState: function() {
+		return {
+			dismissed: false,
+			subscribed: false
+		};
+	},
+
+	subscribe: function() {
+		this.props.pushNotifications.subscribe( ( state ) => {
+			if ( 'subscribed' === state ) {
+				this.setState( { subscribed: true } );
+			}
+		} );
+	},
+
+	dismissNotice: function() {
+		this.setState( { dismissed: true } );
+	},
+
+	pushDeniedNotice: function() {
+		var noticeText = (
+			<div>
+				<p>
+					<strong>Permission denied</strong>
+				</p>
+				<p>Instructions on how to enabled notifications?</p>
+			</div>
+		);
+
+		return <Notice text={ noticeText } className="email-verification-notice" onDismissClick={ this.dismissNotice } />;
+	},
+
+	pushSubscribedNotice: function() {
+		var noticeText = (
+			<div>
+				<p>
+					<strong>Subscription Successful</strong>
+				</p>
+				<p>Instructions on where to go to manage the setting in the future?</p>
+			</div>
+		);
+
+		return <Notice text={ noticeText } className="email-verification-notice" onDismissClick={ this.dismissNotice } />;
+	},
+
+	pushUnsubscribedNotice: function() {
+		var noticeText = (
+			<div>
+				<p>
+					<strong>{ this.translate( 'Don\'t miss out' ) }</strong>
+				</p>
+				<p>
+					{ this.translate( 'Enable browser notifications to instantly receive notice of comments or likes on your posts.' ) }
+				</p>
+				<p>
+					{ this.translate(
+						'{{enableButton}}Turn on browser notifications{{/enableButton}}\xa0\xa0\xa0' +
+						'{{dismissButton}}Not now{{/dismissButton}}', {
+							components: {
+								enableButton: <button className="button" onClick={ this.subscribe } />,
+								dismissButton: <button className="button is-link" onClick={ this.dismissNotice } />
+							} }
+					) }
+				</p>
+			</div>
+		);
+
+		return <Notice text={ noticeText } className="email-verification-notice" onDismissClick={ this.dismissNotice } />;
+	},
+
+	render: function() {
+		var pushNotifications = this.props.pushNotifications;
+
+		if ( this.state.dismissed ) {
+			return null;
+		}
+
+		if ( this.state.subscribed ) {
+			return this.pushSubscribedNotice();
+		}
+
+		if ( 'unknown' === pushNotifications.state || 'subscribed' === pushNotifications.state ) {
+			return null;
+		}
+
+		if ( 'denied' === pushNotifications.state ) {
+			return this.pushDeniedNotice();
+		}
+
+		if ( 'unsubscribed' === pushNotifications.state ) {
+			return this.pushUnsubscribedNotice();
+		}
+
+		return null;
+	}
+} );
